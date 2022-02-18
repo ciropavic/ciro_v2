602e2ca77f1905632b15584262b0e54b3c3be7fa
Matthew Pandolfo <matt@pandolfo.com>
2022-02-18 14:56:03 -0500

    updated vehicles with liveries

---
+++ server-data/resources/[Vehicles]/ecrp-vehicleaudio/fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151',
+  'audioconfig/*.dat151.nametable',
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat10.nametable',
+  'audioconfig/*.dat10.rel',
+  'audioconfig/*.dat10',  
+  'audioconfig/*.dat54.nametable',
+  'audioconfig/*.dat54.rel',
+  'audioconfig/*.dat54',
+  'audioconfig/*.dat',
+  'sfx/**/*.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/v6audiea839_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/v6audiea839_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_v6audiea839'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/pgt3f6_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/pgt3f6_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_pgt3f6'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/tagt3flat6_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/tagt3flat6_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tagt3flat6'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/toysupmk4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/toysupmk4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_toysupmk4'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/alfa690t_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/alfa690t_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_alfa690t'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lambov10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lambov10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lambov10'
